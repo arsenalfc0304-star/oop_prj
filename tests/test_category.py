e6172d5d4253d46287746fbc6ac29c4cb160fcaa
Дмитрий Рыбин <arsenalfc0304@gmail.com>
2026-01-20 12:32:30 +0500

    hw_14_1 in progress

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,8 +1,10 @@
-def test_category_init(category1, product1, product2, product3):
-    assert category1.name == "Смартфоны"
+def test_category_init(category_1, category_2):
+    assert category_1.name == "Смартфоны"
     assert (
-            category1.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+            category_1.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
-    # assert category1.products == [product1, product2, product3]
-    # assert category1.category_count == 5
-    # assert category1.product_count == 5
+    assert category_1.category_count == 2
+    #assert category_1.product_count == 5
+
+    assert category_2.category_count == 2
+    #assert category_2.product_count == 5
