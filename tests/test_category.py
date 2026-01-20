69d7298f056aca1afa760e41cd9cc4e41bb56eea
Дмитрий Рыбин <arsenalfc0304@gmail.com>
2026-01-20 12:56:59 +0500

    hw_14_1 in progress

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,8 +1,11 @@
-def test_category_init(category_1):
+def test_category_init_count(category_1, category_2):
     assert category_1.name == "Смартфоны"
     assert (
             category_1.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
-    assert category_1.category_count == 1
+    assert category_2.category_count == 2
+
+
+def test_category_product_count(category_1):
     assert category_1.product_count == 3
 
