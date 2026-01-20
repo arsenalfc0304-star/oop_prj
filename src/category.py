568aaa7d3f11a5cc0997b9d968837ede2b1257a3
Дмитрий Рыбин <arsenalfc0304@gmail.com>
2026-01-20 10:41:08 +0500

    hw_14_1 in progress

--- src/category.py
+++ src/category.py
@@ -1,8 +1,10 @@
 class Category:
     name: str
     description: str
+    products: list
 
 
-    def __init__(self, name, description):
+    def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
+        self.products = products
