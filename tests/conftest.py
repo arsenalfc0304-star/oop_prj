6886ee351b0ed142e7e7b4ba0d0d1532dd6c3c8a
Дмитрий Рыбин <arsenalfc0304@gmail.com>
2026-01-20 12:16:47 +0500

    hw_14_1 in progress

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,34 +4,34 @@ from src.category import Category
 
 
 @pytest.fixture
-def product1():
+def product_1():
     return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 
 
 @pytest.fixture
-def product2():
+def product_2():
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 
 
 @pytest.fixture
-def product3():
+def product_3():
     return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
 
 @pytest.fixture
-def product4():
+def product_4():
     return Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
 
 
 @pytest.fixture
-def category1():
+def category_1():
     return Category("Смартфоны",
                          "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                         [product1, product2, product3])
+                         [Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5), product_2(), product_3])
 
 
 @pytest.fixture
-def category2():
+def category_2():
     return Category("Телевизоры",
                          "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-                         [product4])
+                         [Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)])
