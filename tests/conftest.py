850e9a9fafa921ad911f185ebe94ae42c5e8e2af
Дмитрий Рыбин <arsenalfc0304@gmail.com>
2026-01-20 11:19:54 +0500

    hw_14_1 in progress

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,20 @@ from src.category import Category
 
 
 @pytest.fixture
-def test_product1():
+def product1():
     return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 
 
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+@pytest.fixture
+def product2():
+    return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+
+@pytest.fixture
+def product3():
+    return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+
+@pytest.fixture
+def product4():
+    return Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
