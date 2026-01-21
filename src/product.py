9c05315e5a6aac1117313230d1d5854e138df7c6
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-01-21 22:58:17 +0500

    hw 14_2 in progress

--- src/product.py
+++ src/product.py
@@ -9,3 +9,7 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_dict):
+        return cls(name=product_dict['name'], description=product_dict['description'], price=product_dict['price'], quantity=product_dict['quantity'])
