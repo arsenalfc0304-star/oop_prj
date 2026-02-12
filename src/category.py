fe94da2d5a3d49900a1706c2ef897f166a17e902
Дмитрий Рыбин <arsenalfc0304@gmail.com>
2026-02-12 22:16:43 +0500

    hw_16_1 in progress

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
@@ -14,11 +17,11 @@ class Category:
 
     def add_product(self, product):
         if product not in self.__products:
-            if isinstance(product, Category):
+            if isinstance(product, Product):
                 self.__products.append(product)
             else:
                 raise TypeError
-        Category.product_count += 1
+            Category.product_count += 1
 
     @property
     def products(self):
