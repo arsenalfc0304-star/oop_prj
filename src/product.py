302661198b92e5f9e3b2f60d058b71ceb8f1fc7b
Дмитрий Рыбин <arsenalfc0304@gmail.com>
2026-02-14 15:47:42 +0500

    hw_16_2 in progress

--- src/product.py
+++ src/product.py
@@ -32,7 +32,7 @@ class PrintMixin:
 
 
     def __repr__(self):
-        return f"{self.__class__.__name__}{self.description}{self.price}{self.quantity}"
+        return f"{self.__class__.__name__}({self.description}{self.price}{self.quantity})"
 
 class Product(BaseProduct, PrintMixin):
     name: str
